d315189c9bc6f07b77167a3c39b3de34b75cf0a5
Jompol CHUENRAROM <78734499+JOMPOL-C@users.noreply.github.com>
2025-10-14 00:27:44 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+console.log("OMISE_PUBLIC_KEY loaded?", !!process.env.OMISE_PUBLIC_KEY);
+console.log("OMISE_SECRET_KEY loaded?", !!process.env.OMISE_SECRET_KEY);
+
+const path = require("path");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const fs = require('fs');
+const cloudinary = require('cloudinary').v2;
+const pagerender = require('./src/utils/pagerender');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+/* ---------- Middlewares ---------- */
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// load .env
+require('dotenv').config();
+
+
+// cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
+
+
+/* ---------- View Engine (EJS) ---------- */
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+/* ---------- Routers ---------- */
+fs.readdirSync(path.join(__dirname, "src/routers"))
+  .filter(file => file.endsWith(".js"))
+  .forEach((file) => {
+    const route = require(path.join(__dirname, "src/routers", file));
+    console.log("👉 Loaded file:", file);
+    app.use("/api", route);
+  });
+
+/* ---------- Page render (EJS pages) ---------- */
+
+
+/* ---------- Start ---------- */
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
---
+++ src/utils/pagerender.js
@@ -0,0 +1,5 @@
+function redderPage(view, title) {
+    return (req, res,) => res.render(view, { title });
+}
+
+/* ---------- Page render (EJS pages) ---------- */
